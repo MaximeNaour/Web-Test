bceb6adea85f0446dc7f085114787c53a0f53060
Maxime Naour <naour.maxime@gmail.com>
2023-10-09 13:48:14 +0000

    Changing Interface

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Maxime Naour",
+  title: "Microbiome Data Scientist",
+  email: "maxime.naour@inrae.fr",
+  gitHub: "https://github.com/MaximeNaour",
+  instagram: "NA",
+  linkedIn: "NA",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "NA",
+  youTube: "NA",
 };
 
 const primaryColor = "#4E567E";
